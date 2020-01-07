88ffb0323fde49a6ee96400868088fd1c632618f
Roman <3romanski@gmail.com>
2020-01-07 10:38:53 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Configuration;
+using System.Windows.Controls;
+using Mannote.Pages;
+
+namespace Mannote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Page InfoPage;
+        Page StatisticPage;
+        Page MainPage;
+        Page DocumentsPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainPage = new Main();
+            CurrentPage.Content = MainPage;
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            if (InfoPage == null) InfoPage = new Info();
+            CurrentPage.Content = InfoPage;
+        }
+
+        private void Statistic_Click(object sender, RoutedEventArgs e)
+        {
+            if (StatisticPage == null) StatisticPage = new Statistic();
+            CurrentPage.Content = StatisticPage;
+        }
+
+        private void Document_Click(object sender, RoutedEventArgs e)
+        {
+            if (DocumentsPage == null) DocumentsPage = new Document();
+            CurrentPage.Content = DocumentsPage;
+        }
+
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage.Content = MainPage;
+        }
+    }
+}
---
+++ Pages/Info.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Mannote
+{
+    /// <summary>
+    /// Interaction logic for Info.xaml
+    /// </summary>
+    public partial class Info : Page
+    {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable uchTable;
+
+        public Info()
+        {
+            InitializeComponent();
+            // получаем строку подключения из app.config
+            connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM [dbo].[UCHDGP_1]";
+            uchTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                connection.Open();
+                adapter.Fill(uchTable);
+                uchGrid.ItemsSource = uchTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+    }
+}
