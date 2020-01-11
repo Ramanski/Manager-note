e66f687a883071a783c9f4f0a84dd0ee44076a5e
Roman <3romanski@gmail.com>
2020-01-11 15:02:48 +0300

    Getting of the information from the DB -- test

--- Pages/Statistic.xaml.cs
+++ Pages/Statistic.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,53 @@ namespace Mannote
     /// </summary>
     public partial class Statistic : Page
     {
+        SqlCommand myCommand;
+        ArrayList arrayList;
+
         public Statistic()
         {
             InitializeComponent();
+        }
+
+        void connectDB()
+        {
+            // Получение строки подключения и поставщика из *.config
+            string provider = ConfigurationManager.AppSettings["provider"];
+            string connectionStr = ConfigurationManager.AppSettings["conStr"];
+            // Создание открытого подключения
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = connectionStr;
+                try
+                {
+                    //Открыть подключение
+                    cn.Open();
+                    // Создание объекта команды с помощью конструктора
+                    string strSQL = "Select * From [Stations]";
+                    myCommand = new SqlCommand(strSQL, cn);
+                    //Выполнение запроса
+                    SqlDataReader dr = myCommand.ExecuteReader();
+                    while (dr.Read())
+                        arrayList.Add(dr.GetString(1));
+                }
+                catch (SqlException ex)
+                {
+                    // Протоколировать исключение
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    // Гарантировать освобождение подключения
+                    cn.Close();
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList = new ArrayList();
+            connectDB();
+            comboBox.ItemsSource = arrayList;
         }
     }
 }
