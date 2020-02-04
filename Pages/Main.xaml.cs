bebafb9a7f9ae3730aaf6490b94b439aac59852f
Roman <3romanski@gmail.com>
2020-02-04 16:30:01 +0300

    Design improved

--- Pages/Main.xaml.cs
+++ Pages/Main.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static CodeFirst.Administration;
 
 namespace Mannote.Pages
 {
@@ -23,6 +24,8 @@ namespace Mannote.Pages
         public Main()
         {
             InitializeComponent();
+            if (App.user != null)
+                tbWelcome.Text = $"Добро пожаловать, {App.user.FirstName} {App.user.SurName}!";
         }
     }
 }
